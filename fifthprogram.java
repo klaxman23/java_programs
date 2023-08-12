afc507cdaaa11bba98201d80f0295bfd7ca8a3c1
Laxman <109780644+klaxman23@users.noreply.github.com>
2023-08-12 08:49:37 +0530

    newfileadd
    newfileadd

---
+++ fifthprogram.java
@@ -0,0 +1,11 @@
+public interface fifthprogram {
+    public static void main(String[] args) {
+        System.out.println("\nName\tHeight\tGender");
+        System.out.println("=======================");
+        System.out.println("Kavya\t2.9\tFemale");
+        System.out.println("Laxman\t5.9\tMale");
+        System.out.println("Suma\t5.5\tFemale");
+
+
+    }
+}
